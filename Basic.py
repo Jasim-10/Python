c8cc864c57118e3fcb2e068bab8c2dfaf2bedf33
Md Jasim Ansari <jasimcec2025@gmail.com>
2025-08-30 12:20:30 +0530

    py

---
+++ Basic.py
@@ -0,0 +1,115 @@
+# hello world
+# print("hello world!")
+# print("Md Jasim Ansari \nMy roll number is 210")
+
+
+
+# #storing variables
+# #1.String
+# name = "Isha"
+# print(type(name))
+
+# #2.Integer
+# roll_number = 17
+# print(type(roll_number))
+
+# #3.floating number
+# percentage = 90.3
+# print(type(percentage))
+
+# #4.bolean
+# is_student = True
+# print(type(is_student))
+
+# print(name,roll_number,percentage,is_student)
+# print("My name is " + name + " and my roll number is ",roll_number)
+
+# #print with seperator
+# print(name, roll_number,percentage,is_student, sep="-")
+
+
+
+
+# print(ord("A"))
+# print(ord("3"))
+# print(ord("अ"))
+# print(ord("😊"))
+# print(chr(85))
+
+
+
+# name = input("Enter your name: ")
+# print("My name is ",name)
+
+# roll_number = input("Enter your roll: ")
+# print(roll_number)
+
+# roll_number = int(input("Enter your roll: "))
+# print(roll_number)
+# print(type(roll_number))
+
+
+
+# num1 = int(input("enter first number: "))
+# num2 = int(input("enter second number: "))
+# sum = num1+num2
+# print(sum)
+
+#Arithmetic Operator
+# number1 = int(input("Enter the first number: "))
+# number2 = int(input("Enter the Second number: "))
+# print("Addition is: ", number1 + number2)
+# print("Subtraction is: ", number1 - number2)
+# print("Multiplication is: ", number1 * number2)
+# print("Division is: ", number1 / number2)
+# print("Remainder is: ", number1 % number2)
+# print("Exponent is: ", number1 ** number2)
+# print("floor division is: ", number1 // number2)
+
+
+
+#Assignment Operator
+# x = 6
+# x += 2
+# print(x)
+# x -= 2
+# print(x)
+# x *= 2
+# print(x)
+# x /= 2
+# print(x)
+# x //= 2
+# print(x)
+# x %= 2
+# print(x)
+# x **= 2
+# print(x)
+# x += 2
+# print(x)
+
+
+
+
+# Comparison Operators
+# a = 5
+# b = 3
+# print("a = ",a)
+# print("b = ",b)
+# print("a == b:", a == b)    
+# print("a != b:", a != b)    
+# print("a > b:", a > b)      
+# print("a < b:", a < b)      
+# print("a >= b:", a >= b)    
+# print("a <= b:", a <= b)    
+
+
+
+#Logical operaotr
+# x = 5
+# y = 10
+# # AND
+# print(x > 0 and y > 0)   # True, both positive
+# # OR
+# print(x < 0 or y > 0)    # True, y is positive
+# # NOT
+# print(not(x > y))         # True, x > y is False, not reverses it

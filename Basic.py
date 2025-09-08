6d4cbf001b5543690b6363e207f7ee092947b4c7
Md Jasim Ansari <jasimcec2025@gmail.com>
2025-09-08 23:59:55 +0530

    py

--- Basic.py
+++ Basic.py
@@ -148,11 +148,11 @@
 
 
 #Bitwise Operator
-a = 5   
-b = 3  
-print("a & b =", a & b)   # 1
-print("a | b =", a | b)   # 7
-print("a ^ b =", a ^ b)   # 6
-print("~a =", ~a)         # -6
-print("a << 1 =", a << 1) # 10
-print("a >> 1 =", a >> 1) # 2
+# a = 5   
+# b = 3  
+# print("a & b =", a & b)   # 1
+# print("a | b =", a | b)   # 7
+# print("a ^ b =", a ^ b)   # 6
+# print("~a =", ~a)         # -6
+# print("a << 1 =", a << 1) # 10
+# print("a >> 1 =", a >> 1) # 2

77c8a678e66fa2a8ae5402287657d121549faba2
Md Jasim Ansari <jasimcec2025@gmail.com>
2025-09-19 23:44:48 +0530

    py

---
+++ collections.py
@@ -0,0 +1,65 @@
+# lists
+# fruits = ["apple","banana","mango","cherry"]
+# print(fruits)   #print list
+# print(type(fruits))   #type of fruits
+# print(len(fruits))   #length of fruit
+# print(fruits[2])   #fruit at 2nd index
+
+
+
+#check if an item is not available in list or not
+# fruits = ["apple","banana","mango","cherry"]
+# if "banana" in fruits:
+#     print ("banana is part of list")
+# if "kiwi" in fruits:
+#     print ("kiwi is part of list")
+# else:
+#     print ("kiwi is not part of list")
+
+
+#add element in list
+# fruits.append("kiwi")
+# print(fruits)
+
+
+#remove element from list
+# fruits.remove("apple")
+# print(fruits)
+
+
+#insert element in list
+# fruits.insert(2,"orange")
+# print(fruits)
+
+
+#extend list
+# morefruits = ["papaya","grapes","watermelon"]
+# fruits.extend(morefruits)
+# print(fruits)
+
+
+#remove element from list
+fruits = ["apple", "banana", "mango", "banana"]
+# fruits.remove("banana") 
+# print(fruits)
+
+# fruits.pop(2)
+# print(fruits)
+
+# fruits.pop()
+# print(fruits)
+
+
+# fruits = ["apple", "banana", "mango", "orange"]
+# fruits[1] = "grapes"          # Change item at index 1
+# print(fruits)
+
+
+
+# fruits = ["apple", "banana", "mango", "orange", "kiwi"]
+# print(fruits)
+# fruits[1:4] = ["grapes", "pineapple", "papaya"]                  # Change items from index 1 to 3 (banana, mango, orange)
+# print(fruits)
+
+
+
